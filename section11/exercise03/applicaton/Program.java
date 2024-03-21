08594e7f251e5b73c36f9d7dc27f231c73d7dc9b
miguelmilagres <miguelmdemacedo@gmail.com>
2024-03-21 11:06:46 -0300

    section11's exercices finished

---
+++ section11/exercise03/applicaton/Program.java
@@ -0,0 +1,63 @@
+package applicaton;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Client;
+import model.entities.Order;
+import model.entities.OrderItem;
+import model.entities.Product;
+import model.enums.OrderStatus;
+
+class Program {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		System.out.println("Enter client data:");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Email: ");
+		String email = sc.nextLine();
+		System.out.print("Birth data (DD/MM/YYYY): ");
+		LocalDate birthDate = LocalDate.parse(sc.nextLine(), fmt);
+		Client client = new Client(name, email, birthDate);
+		
+		System.out.println("Enter order data:");
+		System.out.print("Status: ");
+		OrderStatus status = OrderStatus.valueOf(sc.next());
+		
+		Order order = new Order(LocalDateTime.now(), status, client);
+		
+		System.out.print("How many items to this order? ");
+		int n = sc.nextInt();
+		for (int i = 1; i <= n; i++) {
+			System.out.println("Enter #" + i + " item data:");
+			System.out.print("Product name: ");
+			sc.nextLine();
+			String productName = sc.nextLine();
+			System.out.print("Product price: ");
+			Double productPrice = sc.nextDouble();
+			
+			Product product = new Product(productName, productPrice);
+			
+			System.out.print("Quantity: ");
+			int quantity = sc.nextInt();
+			
+			OrderItem orderItem = new OrderItem(quantity, productPrice, product);
+			
+			order.addItem(orderItem);
+		}
+		
+		System.out.println();
+		System.out.println("ORDER SUMMARY:");
+		System.out.println(order);
+		
+		sc.close();
+	}	
+}
+
---
+++ section11/exercise03/model/enums/OrderStatus.java
@@ -0,0 +1,10 @@
+package model.enums;
+
+public enum OrderStatus {
+	
+	PENDING_PAPYMENT,
+	PROCESSING,
+	SHIPPED,
+	DELIVERED;
+	
+}
